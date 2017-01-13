89b3bdd9e925a5a6a265292da214a07caa693d81
Robert Lehmann <robert.lehmann@sitec-systems.de>
2017-01-13 09:41:38 +0100

    doc: Add license header

--- doc.go
+++ doc.go
@@ -1,3 +1,7 @@
-// Package gmodem handles the connection to a serial cellular modem
-// and the user is able to
+// Copyright (c) 2017 sitec systems GmbH
+// All rights reserved. Use of this source code is goverened by a
+// BSD-style license that can be found in the license file
+
+// Package gmodem provides simple primitives to open a connection to a serial
+// modem device and communicate with it by AT-Commands
 package gmodem
